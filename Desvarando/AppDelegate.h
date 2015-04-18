35477486187975d4139b4c3ac381fb4ca0607921
Yeison Andres Restrepo <yarg375@gmail.com>
2015-04-17 21:18:15 -0500

    Initial Commit

---
+++ Desvarando/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Desvarando
+//
+//  Created by Usuario Mac on 17/04/15.
+//  Copyright (c) 2015 Yeison Restrepo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Desvarando/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Desvarando
+//
+//  Created by Usuario Mac on 17/04/15.
+//  Copyright (c) 2015 Yeison Restrepo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
